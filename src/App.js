89543569003a7fd223608fa38c34da5c5b213205
Elliott Jones <jones.elliott@outlook.com>
2020-11-09 17:20:12 -0800

    set up basic layout & components folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
+import Card from '../src/components/card/card'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Employee Directory
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link">
+          Employee management at the tip of your finger
+        </p>
       </header>
+      <section className="CardSection">
+        <Card />
+      </section>
     </div>
   );
 }
