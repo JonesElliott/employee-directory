8ca5ee49473c30df92b224761a2e378b71520296
Elliott Jones <jones.elliott@outlook.com>
2020-11-14 09:03:48 -0800

    Refactored/reorganized code in table.js and app.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,48 @@ import './App.css';
 class EmployeeResults extends Component {
 
   state = {
-    results: []
+    results: [],
+    filteredEmployees: this.results,
+    sortedEmployees: this.filteredEmployees,
+    filterState: "",
+    sortDirection: 1
   };
 
-  componentDidMount() {
-    this.retrieveEmployeeData();
-  }
 
   retrieveEmployeeData() {
     API.search()
     .then(res => this.setState({ results: res.data.results }))
     .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+
+    this.retrieveEmployeeData();
+
+    let filteredRecords = this.state.results;
+    if (this.filterState !== '') {
+      filteredRecords = this.state.results.filter(employeeRecord => {
+        return employeeRecord.name.first.startsWith(this.state.filterState)
+        || employeeRecord.name.last.startsWith(this.state.filterState);
+      })
+    }
+    
+    this.setState({ filteredEmployees: filteredRecords });
+
+    const filteredEmployeesCopy = this.state.results;
+    filteredEmployeesCopy.sort(compareNames(this.state.sortDirection));
+    this.setState({ soretedEmployees: filteredEmployeesCopy });
+  }
+
+  onFilterChange(event) {
+      const { value } = event.target;
+      this.setState({ filterState: value });
+  }
+
+  toggleSortDirection(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.state.setState({ sortDirection: -1 * this.state.sortDirection });
   }
 
   render() {
@@ -34,7 +65,7 @@ class EmployeeResults extends Component {
         </header>
         <div className="test">
           <section className="TableSection">
-            <TableBody results={this.state.results} />
+            <TableBody results={this.state.results} toggleSortDirection={this.toggleSortDirection}/>
           </section>
         </div>
       </div>
--- src/components/table/tablebody.js
+++ src/components/table/tablebody.js
@@ -1,61 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import compareNames from '../../utils/compareNames';
-import NameFilterInput from '../../utils/nameFilterInput';
-import NameSortLink from '../../utils/namesSortLink';
-import Buttons from '../buttons/buttons';
 import TableHead from './tablehead';
 import TableRow from './tableRow';
 import './table.css';
 
 function TableBody(props) {
 
-    // This becomes "this.state = {}"
-    const [filteredEmployees, setFilteredEmployees] = useState(props.results);
-    const [sortedEmployees, setSortedEmployees] = useState(filteredEmployees);
-    const [filterState, setFilterState] = useState("");
-    const [sortDirection, setSortDirection] = useState(1);
-
-    // Use effect goes from useEffect to componentDidMount (for startup)
-    // and your change function for each update
-
-    useEffect(function () {
-        let filteredRecords = props.results;
-        if (filterState !== '') {
-            filteredRecords = props.results
-                .filter(employeeRecord => {
-                    return employeeRecord.name.first.startsWith(filterState)
-                        || employeeRecord.name.last.startsWith(filterState);
-                })
-        }
-        setFilteredEmployees(filteredRecords);
-    }, [props.results, filterState]);
-    
-    useEffect(function () {
-        const filteredEmployeesCopy = filteredEmployees.slice(0);
-        filteredEmployeesCopy.sort(compareNames(sortDirection));
-        setSortedEmployees(filteredEmployeesCopy);
-    }, [filteredEmployees, sortDirection]);
-
-    // These stay basically the same. They just use this.setState
-    function onFilterChange(event) {
-        const { value } = event.target;
-        setFilterState(value);
-    }
-
-    function toggleSortDirection(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        setSortDirection(-1 * sortDirection);
-    }
-
     return (
         <div className="test">
-            <NameFilterInput filterState={filterState} onFilterChange={onFilterChange}></NameFilterInput>
+            {/* <NameFilterInput filterState={filterState} onFilterChange={onFilterChange}></NameFilterInput> */}
             <table id="employees">
-                <TableHead toggleSortDirection={toggleSortDirection}/>
+                <TableHead toggleSortDirection={props.toggleSortDirection}/>
                 <tbody>
                     {
-                        sortedEmployees.map(employeeRecord =>
+                        props.results.map(employeeRecord =>
                             (<TableRow
                                 employeeRecord={employeeRecord}
                                 key={employeeRecord.id.value} />))
